c2b688eb464c9ae2c2c1e5992b988f1445f24801
komatsutomoya <tosironn@icloud.com>
2020-03-27 09:32:17 +0900

    解答

--- C001-radio.php
+++ C001-radio.php
@@ -1,45 +1,74 @@
-<form name="radioB">
+
+
+<form name="radioB[]">
+  <?php
+$city=["オタワ",
+"トロント",
+"モントリオール",
+"ジュネーブ",
+"チューリッヒ","
+ベルン",
+"ハンブルク",
+"ブレーメン",
+"ベルリン",
+"バルセロナ",
+"マドリード",
+"リスボン",
+"シドニー",
+"メルボルン",
+"キャンベラ"];
+
+$okuni = ["カナダ","スイス","ドイツ","スペイン","オーストラリア"];
+$c=0;
+foreach ($city as $key => $value) {
+  if($key % 3 ==0){
+  echo $okuni[$c++],"の首都は?<br>";}
+  echo "<input type ='radio' name='Q$c' value='$key'>$value<br>";
+  
+}
+
+?>
+
+
 カナダの首都は？<br>
-<input type="radio" name="Q1">オタワ<br>
-<input type="radio" name="Q1">トロント<br>
-<input type="radio" name="Q1">モントリオール<br>
+<label><input type="radio" name="Q1" value="0" checked="checked">オタワ</label><br>
+<label><input type="radio" name="Q1" value="1">トロント</label><br>
+<label><input type="radio" name="Q1" value="2">モントリオール</label><br>
 <br>
 スイスの首都は？<br>
-<input type="radio" name="Q2">ジュネーブ<br>
-<input type="radio" name="Q2">チューリッヒ<br>
-<input type="radio" name="Q2">ベルン<br>
+<label><input type="radio" name="Q2" value="3" checked="checked">ジュネーブ </label><br>
+<label><input type="radio" name="Q2" value="4">チューリッヒ</label><br>
+<label><input type="radio" name="Q2" value="5">ベルン</label><br>
 <br>
 ドイツの首都は？<br>
-<input type="radio" name="Q3">ハンブルク<br>
-<input type="radio" name="Q3">ブレーメン<br>
-<input type="radio" name="Q3">ベルリン<br>
+<label><input type="radio" name="Q3" value="6" checked="checked">ハンブルク</label><br>
+<label><input type="radio" name="Q3" value="8">ブレーメン</label><br>
+<label><input type="radio" name="Q3" value="8">ベルリン</label><br>
 <br>
 スペインの首都は？<br>
-<input type="radio" name="Q4">バルセロナ<br>
-<input type="radio" name="Q4">マドリード<br>
-<input type="radio" name="Q4">リスボン<br>
+<label><input type="radio" name="Q4" value="9" checked="checked">バルセロナ</label><br>
+<label><input type="radio" name="Q4" value="10">マドリード</label><br>
+<label><input type="radio" name="Q4" value="11">リスボン</label><br>
 <br>
 オーストラリアの首都は？<br>
-<input type="radio" name="Q5">シドニー<br>
-<input type="radio" name="Q5">メルボルン<br>
-<input type="radio" name="Q5">キャンベラ<br>
+<label><input type="radio" name="Q5" value="12" checked="checked">シドニー</label><br>
+<label><input type="radio" name="Q5" value="13">メルボルン</label><br>
+<label><input type="radio" name="Q5" value="14">キャンベラ</label><br>
 <br>
-<input type="button" value="採点" onclick="saiten()" />
+<input type="submit" value="採点" name="submit" />
 </form>
 
-
-
-<script>
-function saiten(){
-  var seikai=0; //正解数を入れる変数
-  //答えの番号を配列に入れる
-  var trueAns = new Array(0,5,8,10,14);
-  //正解のラジオボタンがチェックされているか確認
-  for (i=0 ; i<5 ; i++)  {
-    if( document.radioB.elements[trueAns[i]].checked )
-      seikai++;
+<?php
+if(isset($_REQUEST["submit"])){ //もしname=サブミットに値が入ったら⇒採点ボタンを押したら
+  $seikai = 0;                  //変数seikaiを作る。
+  $trueans = [0,5,8,10,14];     //配列tureansを作る。
+  for ($i=0; $i < 5; $i++) { //5回繰り返す
+    $q = $i+1; //変数＄qを作り$iが増えるたびに1増える。 0回目のチェックでQ1を示す。
+    if (isset($_REQUEST["Q$q"])&&$_REQUEST["Q$q"] == $trueans[$i]) { //もしQ＄qのvalueの数字が$trueansと一致していたら
+      $seikai++;
+    }
   }
   
-  alert("あなたは"+seikai*20+"点でした！");
+  echo "あなたは",$seikai*20,"点でした";
 }
-</script>
+  ?>
